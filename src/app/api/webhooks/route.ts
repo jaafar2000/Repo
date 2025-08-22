ea5ed7f8bcc39bb3329adeece2ee76e60d5f49f5
jaafar2000 <jaafar200021@gmail.com>
2025-08-22 21:33:36 +0400

    asdf

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,4 +1,3 @@
-// app/api/webhooks/route.ts
 import { verifyWebhook } from "@clerk/nextjs/webhooks";
 import { NextRequest } from "next/server";
 
@@ -6,16 +5,22 @@ export async function POST(req: NextRequest) {
   try {
     const evt = await verifyWebhook(req);
 
-    console.log("✅ Webhook verified:", evt.type, evt.data);
+    // Do something with payload
+    // For this guide, log payload to console
+    const { id } = evt.data;
+    const eventType = evt.type;
+    console.log(
+      `Received webhook with ID ${id} and event type of ${eventType}`
+    );
+    console.log("Webhook payload:", evt.data);
+
+    if (evt.type === "user.created") {
+      console.log("userId:", evt.data.id);
+    }
 
     return new Response("Webhook received", { status: 200 });
   } catch (err) {
-    console.error("❌ Webhook error:", err);
+    console.error("Error verifying webhook:", err);
     return new Response("Error verifying webhook", { status: 400 });
   }
-}
-
-// Block GET requests (important)
-export async function GET() {
-  return new Response("Method Not Allowed", { status: 405 });
 }
